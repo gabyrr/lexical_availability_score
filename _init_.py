cf02585ee82834b28c27b6891bcb5da740f15318
Gabriela Ramirez <gabriela@oppy.mars>
2019-11-12 21:30:17 -0600

    added another module to compute IDLP STANDARIZED as proposed by Callealta Barroso and Gallego Gallego in Medidas de disponibilidad lexica: comparabilidad y normalizacion

--- _init_.py
+++ _init_.py
@@ -1,12 +1,22 @@
-"""An example of use from compute index lexical availability score
+"""An example of use from compute index lexical availability score and index lexical availability standarized score
 
 .. moduleauthor:: Gabriela Ramirez (github/gabyrr)
 
 """
 
 import get_idlv
-
 dirIn="input/"
 dirOut="output/"
-resolution = 2
+resolution = 1
 get_idlv.main(dirIn, dirOut, resolution)
+
+
+import get_idlv_st
+dirIn="input/"
+dirOut="output/"
+resolution = 1
+k=5
+w=0.1
+m=1.0
+
+get_idlv_st.main(dirIn, dirOut, resolution, k, w, m)
